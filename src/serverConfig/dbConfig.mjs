47582ed9253d44794c21ce360fed7c7c9eb46acc
Varga Albert <albi1000@icloud.com>
2023-12-16 15:00:28 +0100

    Delete src/serverConfig directory

--- src/serverConfig/dbConfig.mjs
+++
@@ -1,6 +0,0 @@
-const dbConfig = {
-    mongoURIsharpSystem: "mongodb+srv://albi:Ais6A45SJpDP3n5r@fishing-ecommerce.n8amumh.mongodb.net/sharpSystem?retryWrites=true&w=majority",
-    mongoURItest: "mongodb+srv://albi:Ais6A45SJpDP3n5r@fishing-ecommerce.n8amumh.mongodb.net/test?retryWrites=true&w=majority",
-};
-
-export default dbConfig;
